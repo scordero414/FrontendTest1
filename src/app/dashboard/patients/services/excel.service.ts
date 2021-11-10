bf24e4471691ef378d97614520eb48e1fdb47301
Sebastian Cordero <scordero414@gmail.com>
2021-11-10 01:30:34 -0500

    fix excel export

--- src/app/dashboard/patients/services/excel.service.ts
+++ src/app/dashboard/patients/services/excel.service.ts
@@ -15,10 +15,9 @@ export class ExcelService {
   public exportAsExcelFile(json: any[], excelFileName: string): void {
     
     const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
-    console.log('worksheet',worksheet);
     const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
-    // const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
-    const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'buffer' });
+    const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+    // const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'buffer' });
     this.saveAsExcelFile(excelBuffer, excelFileName);
   }
 
